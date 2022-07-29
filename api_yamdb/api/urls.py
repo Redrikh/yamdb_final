8e77a262eb448c70eba4160fab437ca63b815acc
Евгений Скворцов <skvorec@gmail.com>
2022-07-29 21:59:40 +0300

    test push

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,8 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    CategoryViewSet, CommentViewSet, GenreViewSet,
-    ReviewViewSet, TitleViewSet, UsersViewSet
+    CategoryViewSet,
+    CommentViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    TitleViewSet,
+    UsersViewSet
 )
 
 router = DefaultRouter()
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -4,17 +4,23 @@ from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.pagination import (
-    LimitOffsetPagination, PageNumberPagination
+    LimitOffsetPagination,
+    PageNumberPagination,
 )
 from rest_framework.response import Response
-
 from reviews.filters import TitleFilter
 from reviews.models import Category, Genre, Review, Title
+
 from .permissions import IsAdmin, IsAdminOrReadOnly, IsAuthorOrStaff
 from .serializers import (
-    CategorySerializer, CommentSerializer, GenreSerializer,
-    ReviewSerializer, TitleCreateSerializer, TitleSerializer,
-    UserPatchSerializer, UserSerializer
+    CategorySerializer,
+    CommentSerializer,
+    GenreSerializer,
+    ReviewSerializer,
+    TitleCreateSerializer,
+    TitleSerializer,
+    UserPatchSerializer,
+    UserSerializer
 )
 
 User = get_user_model()
